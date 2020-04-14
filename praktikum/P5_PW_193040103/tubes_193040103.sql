7d16e0d169c3879b7eb9de5c77e9f2a698ee7194
dzakimaulana03 <58681034+dzakimaulana03@users.noreply.github.com>
2020-04-14 17:30:46 +0700

    Praktikum Modul 5
    update sql

--- praktikum/P5_PW_193040103/tubes_193040103.sql
+++ praktikum/P5_PW_193040103/tubes_193040103.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2020 at 09:17 AM
+-- Generation Time: Apr 14, 2020 at 12:23 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
